ca5f631f9aac10cb3e1ecff2eba96c851f0436d5
DevAndresMunoz <andresgabrielmunoz@gmail.com>
2024-10-25 20:32:03 -0600

    changed name of object that stores all game data to allGameData

--- js/high-score.js
+++ js/high-score.js
@@ -1,16 +1,16 @@
-let highScoreList = {};
+let allGameData = {};
 
 function retrieveAllLocalStorage() {
     for (let i = 0; i < localStorage.length; i++) {
         const key = localStorage.key(i);
         try {
             // Try to parse the value and store it in the highScoreList object
-            highScoreList[key] = JSON.parse(localStorage.getItem(key));
+            allGameData[key] = JSON.parse(localStorage.getItem(key));
         } catch (e) {
             // If parsing fails, store it as a plain value - this is the only way I could avoid JSON.parse get "Uncaught SyntaxError: Unexpected token h" error
-            highScoreList[key] = localStorage.getItem(key);
+            allGameData[key] = localStorage.getItem(key);
         }
     }
 }
 retrieveAllLocalStorage();
-console.log(highScoreList)
+console.log(allGameData)
