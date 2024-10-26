c50f3c58dcacb47d6bf4bcf5d2a1e612bfe4336a
DevAndresMunoz <andresgabrielmunoz@gmail.com>
2024-10-26 13:33:43 -0600

    added high score panel which dynamically updates with local storage

--- js/high-score.js
+++ js/high-score.js
@@ -22,4 +22,65 @@ const sortedScores = Object.entries(allGameData).sort(function(a, b) {return b[1
 
 // Save top ten highest scores from sortedScores array and save to highScores array
 const highScores = sortedScores.slice(0,10);
-    
+console.log(highScores)
+
+// Dynamically update high score panel
+const highScorePanel = document.querySelector('.high-score-list');
+highScorePanel.innerHTML = `
+    <div class="high-score-item">
+        <div class="order"><strong>NO.</strong></div>
+        <div class="username"><strong>NAME</strong></div>
+        <div class="score"><strong>SCORE</strong></div>
+    </div>
+    <div class="high-score-item">
+        <div class="order">1</div>
+        <div class="username">${highScores[0][1].username}</div>
+        <div class="score">${highScores[0][1].score}</div>
+    </div>
+    <div class="high-score-item">
+        <div class="order">2</div>
+        <div class="username">${highScores[1][1].username}</div>
+        <div class="score">${highScores[1][1].score}</div>
+    </div>    
+        <div class="high-score-item">
+        <div class="order">3</div>
+        <div class="username">${highScores[2][1].username}</div>
+        <div class="score">${highScores[2][1].score}</div>
+    </div> 
+        <div class="high-score-item">
+        <div class="order">4</div>
+        <div class="username">${highScores[3][1].username}</div>
+        <div class="score">${highScores[3][1].score}</div>
+    </div>  
+        <div class="high-score-item">
+        <div class="order">5</div>
+        <div class="username">${highScores[4][1].username}</div>
+        <div class="score">${highScores[4][1].score}</div>
+    </div>  
+        <div class="high-score-item">
+        <div class="order">6</div>
+        <div class="username">${highScores[5][1].username}</div>
+        <div class="score">${highScores[5][1].score}</div>
+    </div>  
+        <div class="high-score-item">
+        <div class="order">7</div>
+        <div class="username">${highScores[6][1].username}</div>
+        <div class="score">${highScores[6][1].score}</div>
+    </div>  
+        <div class="high-score-item">
+        <div class="order">8</div>
+        <div class="username">${highScores[7][1].username}</div>
+        <div class="score">${highScores[7][1].score}</div>
+    </div>  
+      </div>  
+        <div class="high-score-item">
+        <div class="order">9</div>
+        <div class="username">${highScores[8][1].username}</div>
+        <div class="score">${highScores[8][1].score}</div>
+    </div>    </div>  
+        <div class="high-score-item">
+        <div class="order">10</div>
+        <div class="username">${highScores[9][1].username}</div>
+        <div class="score">${highScores[9][1].score}</div>
+    </div>  
+`;
