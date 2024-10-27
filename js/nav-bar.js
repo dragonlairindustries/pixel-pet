d479a5e0c3fab4146793398763686acd474bc42f
DevAndresMunoz <andresgabrielmunoz@gmail.com>
2024-10-27 14:44:59 -0600

    created adaptive nav bar and added to home page

---
+++ js/nav-bar.js
@@ -0,0 +1,10 @@
+
+function showSidebar() {
+    const sideBar = document.querySelector('.sidebar');
+    sideBar.style.display = 'flex';
+}
+
+function hideSidebar() {
+    const sideBar = document.querySelector('.sidebar');
+    sideBar.style.display = 'none'; 
+}
